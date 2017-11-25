73eea4e8fcda0a73efe5a53a31a1a4093971cf9d
Jerome Leclanche <jerome@leclan.ch>
2017-11-25 16:01:29 +0200

    Add tests for logged out referrals

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 from django_reflinks.models import ReferralLink
 
 
-@pytest.yield_fixture(scope="session")
+@pytest.yield_fixture(scope="function")
 def ref_link():
 	yield ReferralLink.objects.create(identifier="Example123")
--- tests/test_main.py
+++ tests/test_main.py
@@ -32,3 +32,33 @@ def test_referral_logged_in(admin_client, ref_link):
 	assert hit.hit_user == response.wsgi_request.user
 	assert hit.next == next
 	assert not hit.confirmed
+
+
+@pytest.mark.django_db
+def test_referral_logged_out(client, ref_link):
+	ref_link_url = ref_link.get_absolute_url()
+
+	response = client.get(ref_link_url, HTTP_USER_AGENT="TestAgent")
+	assert response.status_code == 302
+	assert response.url == "/"
+
+	assert ReferralHit.objects.count() == 1
+	hit = ReferralHit.objects.latest("created")
+	assert not hit.authenticated
+	assert not hit.hit_user
+	assert hit.ip == "127.0.0.1"
+	assert hit.user_agent == response.wsgi_request.META["HTTP_USER_AGENT"]
+	assert not hit.next
+	assert not hit.confirmed
+
+	next = "/foo"
+	response = client.get(ref_link_url + "?next=" + next)
+	assert response.status_code == 302
+	assert response.url == next
+
+	assert ReferralHit.objects.count() == 2
+	hit = ReferralHit.objects.latest("created")
+	assert not hit.authenticated
+	assert not hit.hit_user
+	assert hit.next == next
+	assert not hit.confirmed
