83606833c813fb462408f4d03503126786a5280a
Jerome Leclanche <jerome@leclan.ch>
2017-12-03 07:30:08 +0200

    Change REFERRAL_COOKIE_MAX_AGE default to 365 days

--- django_reflinks/settings.py
+++ django_reflinks/settings.py
@@ -4,5 +4,5 @@ Settings for django-reflinks
 
 REFERRAL_COOKIE_KEY = "django-reflinks__rk"
 REFERRAL_COOKIE_HTTPONLY = True
-REFERRAL_COOKIE_MAX_AGE = None  # Expires at session end
+REFERRAL_COOKIE_MAX_AGE = 60 * 60 * 24 * 365
 REFERRAL_URL_PARAM = "ref"
